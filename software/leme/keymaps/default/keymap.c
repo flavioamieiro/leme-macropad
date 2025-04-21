fdad7665541bf5cb07bdc872d2bfe7987265c39c
Flavio Amieiro <amieiro.flavio@gmail.com>
2025-04-21 18:44:53 -0300

    Adds second layer to the macropad
    This has the same keycodes used in the previous KMK firmware, which
    means the second layer is only useful to change the rotary encoder into
    a mouse wheel instead of a volume knob. This layer should be expanded
    for more navigation helpers, and this is why I named it as `NAVIGATION`.

--- software/leme/keymaps/default/keymap.c
+++ software/leme/keymaps/default/keymap.c
@@ -3,6 +3,11 @@
 
 #include QMK_KEYBOARD_H
 
+enum layers {
+  _BASE,
+  _NAVIGATION
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┐
@@ -11,14 +16,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │ D │ E │ F │
      * └───┴───┴───┘
      */
-    [0] = LAYOUT_ortho_2x3(
+    [_BASE] = LAYOUT_ortho_2x3(
         KC_F14,    KC_F15,    KC_F16,
-        KC_F17,    KC_F18,    KC_NO
+        KC_F17,    KC_F18,    TG(_NAVIGATION)
+    ),
+    [_NAVIGATION] = LAYOUT_ortho_2x3(
+        _______, _______, _______,
+        _______, _______, TG(_NAVIGATION)
     )
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-  [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
+  [_BASE] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+  [_NAVIGATION] = { ENCODER_CCW_CW(MS_WHLU, MS_WHLD) }
 };
 #endif
