3e506e77ad464149b499ac64293dbf57abaee0c2
Flavio Amieiro <amieiro.flavio@gmail.com>
2025-04-21 18:37:00 -0300

    Fixes copyright statement in keymaps.c

--- software/leme/keymaps/default/keymap.c
+++ software/leme/keymaps/default/keymap.c
@@ -1,5 +1,5 @@
-// Copyright 2023 QMK
-// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright 2025 Flavio Amieiro <amieiro.flavio@gmail.com>
+// SPDX-License-Identifier: GPL-3.0-only
 
 #include QMK_KEYBOARD_H
 
