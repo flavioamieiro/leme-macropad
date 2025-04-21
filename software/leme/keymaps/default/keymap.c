6194ca91f4f59ebbbf7799f07048cb3f900c25a3
Flavio Amieiro <amieiro.flavio@gmail.com>
2025-04-20 23:13:04 -0300

    Changes firmware to use QMK instead of KMK
    This QMK firmware is still very limited in comparrison to what was
    available with KMK.
    
    * It still has only one layer
    * The rotary encoder pushbutton doesn't work yet
    * The display is not yet included

---
+++ software/leme/keymaps/default/keymap.c
@@ -0,0 +1,24 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ A │ B │ C │
+     * ├───┼───┼───┤
+     * │ D │ E │ F │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_2x3(
+        KC_F14,    KC_F15,    KC_F16,
+        KC_F17,    KC_F18,    KC_NO
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+  [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
+};
+#endif
